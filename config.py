64b9b8618f56aca5efd8ac2b999e9d19ed078528
MUFAZ VK <93363575+BXBotz2021@users.noreply.github.com>
2024-12-08 21:00:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config(object):
 
-      BOT_TOKEN = os.environ.get("BOT_TOKEN", "5878840631:AAHDk0LPuwmucMAJrtnM9GnhvUIDO9CLZP8")
+      BOT_TOKEN = os.environ.get("BOT_TOKEN", "8185385351:AAFiRBjdC50laiZfIUNloySQ19QUbU85-Z0")
       API_ID = int(os.environ.get('API_ID', '4088138'))
       API_HASH = os.environ.get('API_HASH', '4b19fd7339c360e956461f0f5535c5b1')
       OWNER_ID = int(os.environ.get("OWNER_ID", "1890385137"))
