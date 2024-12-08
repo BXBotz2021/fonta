e503453c07ade19a296076fa7d8c37b7dd9f0c26
MUFAZ VK <93363575+BXBotz2021@users.noreply.github.com>
2024-12-08 21:29:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 import os
 
 class Config(object):
-
-      BOT_TOKEN = os.environ.get("BOT_TOKEN", "8185385351:AAFiRBjdC50laiZfIUNloySQ19QUbU85-Z0")
-      API_ID = int(os.environ.get('API_ID', '4088138'))
-      API_HASH = os.environ.get('API_HASH', '4b19fd7339c360e956461f0f5535c5b1')
-      OWNER_ID = int(os.environ.get("OWNER_ID", "1890385137"))
-
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8185385351:AAFiRBjdC50laiZfIUNloySQ19QUbU85-Z0")
+    API_ID = int(os.environ.get('API_ID', '4088138'))
+    API_HASH = os.environ.get('API_HASH', '4b19fd7339c360e956461f0f5535c5b1')
+    OWNER_ID = int(os.environ.get("OWNER_ID", "1890385137"))
+    
+    # Add your AUTH_CHANNEL here
+    AUTH_CHANNEL = os.environ.get("AUTH_CHANNEL", "-1002437864651")  # Replace with your channel ID or username
